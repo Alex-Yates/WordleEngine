b5127f6e0c029fcce9a373005cacebfeea929548
Alex Yates <Alex-Yates@users.noreply.github.com>
2022-05-21 20:58:48 +0100

    Now has the capability to add rules and use them to filter a wordlist
    * scaffolding for new Facts feature
    
    * made a bunch of progress, couple of tests still failing
    
    * more tests, fixed bug (missing !)

--- WordleEngine/src/WordleEngine/AllowedWordsList.cs
+++ WordleEngine/src/WordleEngine/AllowedWordsList.cs
@@ -16,20 +16,6 @@ namespace WordleEngine {
             this.AllowedWords = ConvertCSVtoDataTable(legalWordsFile);
         }
 
-        public bool Contains(string input) {
-            bool wordExists = false;
-            string filter = "word = \'" + input + "\'";
-            DataRow[] wordRow = AllowedWords.Select(filter);
-            if(wordRow.Length == 1) {
-                wordExists = true;
-            }
-            return wordExists;
-        }
-
-        public int Count() {
-            return AllowedWords.Rows.Count;
-        }
-
         private static DataTable ConvertCSVtoDataTable(string strFilePath)
         {
             StreamReader sr = new StreamReader(strFilePath);
@@ -50,6 +36,69 @@ namespace WordleEngine {
                 dt.Rows.Add(dr);
             }
             return dt;
+        }
+
+        public bool Contains(string input)
+        {
+            bool wordExists = false;
+            string filter = "word = \'" + input + "\'";
+            DataRow[] wordRow = AllowedWords.Select(filter);
+            if (wordRow.Length == 1)
+            {
+                wordExists = true;
+            }
+            return wordExists;
+        }
+
+        public int Count()
+        {
+            return AllowedWords.Rows.Count;
+        }
+
+        public void ApplyFacts(List<Fact> facts) {
+            foreach (Fact fact in facts) {
+                // Position must either be 0 (position irrelavent) or between 1 and 5 (specific position known)
+                if ((fact.Position > 5) || (fact.Position < 0)){
+                    string errorMsg = "Error!: Fact must have position between -1 and 4";
+                    throw new InvalidOperationException(errorMsg);
+                }
+
+                DataRow[] rowsToKeep = new DataRow[0];
+
+                // Case: where the letter exists but the position is irrelevant
+                if ((fact.GetPosition() == 0) && (fact.GetExists())) {
+                    // keep only the words that contain the letter
+                    string expression = "Word LIKE '*" + fact.GetLetter() + "*'";
+                    rowsToKeep = this.AllowedWords.Select(expression);
+                }
+
+                // Case: where letter does not exist in any position
+                if ((fact.GetPosition() == 0) && !(fact.GetExists())) {
+                    // keep only the words that do not contain the letter
+                    string expression = "Word NOT LIKE '*" + fact.GetLetter() + "*'";
+                    rowsToKeep = this.AllowedWords.Select(expression);
+                }
+
+                // Case: where letter exists in a specific position
+                if ((fact.GetPosition() != 0) && (fact.GetExists())) {
+                    // keep only the words that contain the letter in the specific position
+                    string expression = "SUBSTRING(Word," + fact.GetPosition() + ",1) LIKE '" + fact.GetLetter() + "'";
+                    rowsToKeep = this.AllowedWords.Select(expression);
+                }
+
+                // Case: where letter does not exist in a specific position
+                if ((fact.GetPosition() != 0) && !(fact.GetExists())) {
+                    // keep only the words do not contain the letter in the specific position
+                    string expression = "SUBSTRING(Word," + fact.GetPosition() + ",1) NOT LIKE '" + fact.GetLetter() + "'";
+                    rowsToKeep = this.AllowedWords.Select(expression);
+                }
+
+                // Create new datatable (quicker than iterating through and removing rows individually)
+                DataTable tempDataTable = rowsToKeep.CopyToDataTable();
+                this.AllowedWords.Clear();
+                this.AllowedWords.Merge(tempDataTable);
+                tempDataTable.Dispose();
+            }
         }
     }
 }
---
+++ WordleEngine/src/WordleEngine/Fact.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordleEngine
+{
+    public class Fact
+    {
+        public readonly char Letter;
+        public readonly bool Exists;
+        public readonly int Position;
+
+        public Fact(char letter, bool exists, int position) {
+            if ((position < 0) || (position > 5)) {
+                string errorMsg = "ERROR!: Rule cannot have position " + position + ". Position must be between 0 and 5.";
+                throw new InvalidOperationException(errorMsg);
+            }
+            this.Letter = letter;
+            this.Exists = exists;
+            this.Position = position;
+        }
+
+        public char GetLetter() {
+            return this.Letter;
+        }
+
+        public bool GetExists() {
+            return this.Exists;
+        }
+
+        public int GetPosition() {
+            return this.Position;
+        }
+    }
+}
