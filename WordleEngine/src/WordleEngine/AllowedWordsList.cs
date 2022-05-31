c0bdd04f60c973a2e1ee4ced68608247f8cc4fe0
Alex Yates <alex.yates@dlmconsultants.com>
2022-05-31 19:33:45 +0100

    Builds but tests failing. I think the issue is AllowedWordsList.ConvertCSVToAllowedWordsList

--- WordleEngine/src/WordleEngine/AllowedWordsList.cs
+++ WordleEngine/src/WordleEngine/AllowedWordsList.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.IO;
 using System.Data;
 using System.Text.RegularExpressions;
@@ -10,50 +9,37 @@ namespace WordleEngine {
     
     public class AllowedWordsList {
 
-        private DataTable AllowedWords { get; set; }
+        public List<Word> AllowedWords = new List<Word>();
 
         public AllowedWordsList() {
             var legalWordsFile = @"wordle-allowed-guesses.csv";
-            this.AllowedWords = ConvertCSVtoDataTable(legalWordsFile);
+            this.AllowedWords = ConvertCSVToAllowedWordsList(legalWordsFile);
         }
 
-        private static DataTable ConvertCSVtoDataTable(string strFilePath)
-        {
+        private static List<Word> ConvertCSVToAllowedWordsList(string strFilePath) {
             StreamReader sr = new StreamReader(strFilePath);
             string[] headers = sr.ReadLine().Split(',');
-            DataTable dt = new DataTable();
-            foreach (string header in headers)
-            {
-                dt.Columns.Add(header);
+            List<Word> wordList = new List<Word>();
+
+            while (!sr.EndOfStream) {
+                string[] row = Regex.Split(sr.ReadLine(), ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                string text = row[1];
+                int prevalence = int.Parse(row[2]);
+                Word word = new Word(text, prevalence);
+                wordList.Add(word);
             }
-            while (!sr.EndOfStream)
-            {
-                string[] rows = Regex.Split(sr.ReadLine(), ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
-                DataRow dr = dt.NewRow();
-                for (int i = 0; i < headers.Length; i++)
-                {
-                    dr[i] = rows[i];
-                }
-                dt.Rows.Add(dr);
-            }
-            return dt;
+
+            return wordList;
         }
 
-        public bool Contains(string input)
-        {
-            bool wordExists = false;
-            string filter = "word = \'" + input + "\'";
-            DataRow[] wordRow = AllowedWords.Select(filter);
-            if (wordRow.Length == 1)
-            {
-                wordExists = true;
-            }
-            return wordExists;
+        public bool Contains(string input) {
+            List<Word> matchingWords = AllowedWords.FindAll(i => i.GetName().Equals(input));
+            return (matchingWords.Count == 1);
         }
 
         public int Count()
         {
-            return AllowedWords.Rows.Count;
+            return AllowedWords.Count;
         }
 
         public void ApplyFacts(List<Fact> facts) {
@@ -69,44 +55,39 @@ namespace WordleEngine {
                 // Case: where the letter exists but the position is irrelevant
                 if ((fact.GetPosition() == 0) && (fact.GetExists())) {
                     // keep only the words that contain the letter
-                    string expression = "Word LIKE '*" + fact.GetLetter() + "*'";
-                    rowsToKeep = this.AllowedWords.Select(expression);
+                    List<Word> wordsToKeep = AllowedWords.FindAll(i => i.GetName().Contains(fact.GetLetter()));
+                    AllowedWords = wordsToKeep;
                 }
 
                 // Case: where letter does not exist in any position
                 if ((fact.GetPosition() == 0) && !(fact.GetExists())) {
                     // keep only the words that do not contain the letter
-                    string expression = "Word NOT LIKE '*" + fact.GetLetter() + "*'";
-                    rowsToKeep = this.AllowedWords.Select(expression);
+                    List<Word> wordsToRemove = AllowedWords.FindAll(i => i.GetName().Contains(fact.GetLetter()));
+                    foreach (Word wordToRemove in wordsToRemove){
+                        AllowedWords.Remove(wordToRemove);
+                    }
                 }
 
                 // Case: where letter exists in a specific position
                 if ((fact.GetPosition() != 0) && (fact.GetExists())) {
                     // keep only the words that contain the letter in the specific position
-                    string expression = "SUBSTRING(Word," + fact.GetPosition() + ",1) LIKE '" + fact.GetLetter() + "'";
-                    rowsToKeep = this.AllowedWords.Select(expression);
+                    List<Word> wordsToKeep = AllowedWords.FindAll(i => i.GetName()[fact.GetPosition()].Equals(fact.GetLetter()));
+                    AllowedWords = wordsToKeep;
                 }
 
                 // Case: where letter does not exist in a specific position
                 if ((fact.GetPosition() != 0) && !(fact.GetExists())) {
                     // keep only the words do not contain the letter in the specific position
-                    string expression = "SUBSTRING(Word," + fact.GetPosition() + ",1) NOT LIKE '" + fact.GetLetter() + "'";
-                    rowsToKeep = this.AllowedWords.Select(expression);
+                    List<Word> wordsToRemove = AllowedWords.FindAll(i => i.GetName()[fact.GetPosition()].Equals(fact.GetLetter()));
+                    foreach (Word wordToRemove in wordsToRemove) {
+                        AllowedWords.Remove(wordToRemove);
+                    }
                 }
-
-                // Create new datatable (quicker than iterating through and removing rows individually)
-                DataTable tempDataTable = rowsToKeep.CopyToDataTable();
-                this.AllowedWords.Clear();
-                this.AllowedWords.Merge(tempDataTable);
-                tempDataTable.Dispose();
             }
         }
 
         public string GetTopWord() { 
-            DataRow topRankedWordRow = (DataRow)AllowedWords.Rows.Cast<System.Data.DataRow>().Take(1);
-            string topRankedWord = topRankedWordRow.Field<string>("word");
-
-            return topRankedWord;
+            return AllowedWords[0].GetName();
         }
     }
 }
---
+++ WordleEngine/src/WordleEngine/Word.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordleEngine
+{
+    public class Word
+    {
+        private string Name { get; set; }
+        private  int  Prevelance { get; set; }
+
+        public Word(string name, int prevalence) {
+            this.Name = name;
+            this.Prevelance = prevalence;
+        }
+
+        public string GetName() {
+            return this.Name;
+        }
+
+        public int GetPrevalence() {
+            return this.Prevelance;
+        }
+    }
+}
